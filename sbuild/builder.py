669e48168776b81b91f5724aebbd2039f40a87a5
Zoltan Pallagi <pzolee@balabit.com>
2015-03-31 15:07:49 +0200

    libmongo_client.builder.py: Added LibMongoClientBuilder
    To compile with glib syslog-ng instead of system glib

--- sbuild/builder.py
+++ sbuild/builder.py
@@ -1,7 +1,18 @@
 from AutotoolsBuilder import AutotoolsBuilder
+import os
+
+class LibMongoClientBuilder(AutotoolsBuilder):
+
+    def environment_override(self):
+        self._add_extra_env(
+         {"PKG_CONFIG_PATH" : "%s/lib/pkgconfig"%(self.install_dir),
+          "PATH": os.environ['PATH'] + ":%s/bin"%(self.install_dir),
+         })
+
+
 
 def get_builder():
-    return AutotoolsBuilder(get_default_config_opts())
+    return LibMongoClientBuilder(get_default_config_opts())
 
 def get_default_config_opts():
     try:
@@ -10,3 +21,4 @@ def get_default_config_opts():
         return None
     else:
         return configure_opts.default_opts
+
